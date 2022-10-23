a33d3910b8660a7b2d5061196ebb3a2139ee65e5
EMMANUEL <dammylola299@gmail.com>
2022-10-23 23:59:34 +0100

    Learnt about useState and how to apply it to project

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [name, setName] = useState('Emmanuel');
+    const [age, setAge] = useState('22');
+
+    const handleClick = () => {
+        setName('Damilola');
+        setAge('23');
+    }
+
     return(
         <div className="home">
             <h2>Homepage</h2>
+            <p>My name is {name} and I am {age} years old!</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 }
 
 export default Home;
+
