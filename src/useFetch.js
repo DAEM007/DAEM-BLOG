247aa84bdde2fcd5641a514c578c19b656b8cc7a
EMMANUEL <dammylola299@gmail.com>
2022-12-11 23:35:17 +0100

    made a custom hook

---
+++ src/useFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const useFetch = ((url) => {
+    const [data, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url)
+            .then(res => {
+                if(!res.ok){
+                    throw Error(`couldn't fetch response from the server!!!`);
+                }
+                return res.json();
+            })
+            .then(data => {
+                setError(false);
+                setBlogs(data);
+                setIsPending(false);
+            })
+            .catch((err) => {
+                setIsPending(false);
+                setError(err.message);
+            })
+        }, 3000);
+    }, [url]); 
+
+    return {data, isPending, error};
+
+})
+
+export default useFetch;
