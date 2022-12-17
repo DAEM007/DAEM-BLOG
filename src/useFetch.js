18dfb0bbfd212dde3a2b1ebac0d3f5658a937de2
EMMANUEL <dammylola299@gmail.com>
2022-12-17 23:27:11 +0100

    Added firebase to the custom hook and revised useFetch hook

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,35 +1,32 @@
 import { useState, useEffect } from "react";
 
-const useFetch = ((url) => {
-    const [data, setBlogs] = useState(null);
+// All firebase import here...
+import { db } from "./Firebase";
+import { collection, onSnapshot } from "firebase/firestore";
+
+const useFetch = (col) => {
+    const [documents, setDocuments] = useState(null);
     const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
-        
-        setTimeout(() => {
-            fetch(url)
-            .then(res => {
-                if(!res.ok){
-                    throw Error(`couldn't fetch response from the server!!!`);
-                }
-                return res.json();
-            })
-            .then(data => {
-                setError(false);
-                setBlogs(data);
-                setIsPending(false);
-            })
-            .catch((err) => {
-                setIsPending(false);
-                setError(err.message);
-            })        
-        }, 2000);
-    
-    }, [url]); 
+        const colRef = collection(db, col);
 
-    return {data, isPending, error};
+        const unsub = onSnapshot(colRef, (snapshot) => {
+            let results = [];
+            snapshot.docs.forEach((doc)  => {
+                results.push({id: doc.id, ...doc.data()})
+            })
+            setDocuments(results);
+            setIsPending(false);
+        })
 
-})
+        // chack if you can use the 'then' method on the unsub variable itself which is the onSnapshot...
+
+        return unsub;
+    }, [col])
+
+    return { documents, isPending };
+
+}
 
 export default useFetch;
