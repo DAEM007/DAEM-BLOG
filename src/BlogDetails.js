f17e331344639864ce2dda6d6026464c1866907f
EMMANUEL <dammylola299@gmail.com>
2022-12-17 23:20:45 +0100

    got single document from firebase and revised BlogDetails component

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,24 +1,45 @@
-import { useHistory, useParams } from "react-router-dom";
-import useFetch from "./useFetch";
+import { useEffect, useState } from "react";
+import { useHistory, useParams} from "react-router-dom";
+
+// All firebase imports
+import { db } from "./Firebase";
+import { collection, deleteDoc, doc, onSnapshot } from "firebase/firestore";
+
+
 
 const BlogDetails = () => {
     const { id } = useParams();
-    const { isPending, error, data: blog } = useFetch(` http://localhost:9000/blogs/${id}`);
     const history = useHistory();
+    const [ blog, setBlog ] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+
+    const colRef = collection(db, 'blogs')
+    const docRef = doc(colRef, id)
+
+    useEffect(() => {
+        
+        onSnapshot(docRef, (doc) => {
+            let data = doc.data();
+            console.log(data);
+            setBlog(data);
+            setIsPending(false);
+        })
+
+
+    }, [docRef])
 
     const handleDelete = () =>  {
-        fetch(`http://localhost:9000/blogs/${id}`, {
-            method: 'DELETE',
-        })
-        .then(() => {
-            history.push('/');
-        })
+        deleteDoc(docRef)
+            .then(() => {
+                history.push('/');
+                setIsPending(false);
+            })
+        console.log('Docuent deleted');
     }
 
     return ( 
         <div className="blog-details">
             {isPending && <div>Loading...</div>}
-            {error && <div>{ error }</div>}
             {blog && (
                 <div>
                     <h2>{ blog.title }</h2>
