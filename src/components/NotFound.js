db6bb88ce838609a39b3e0e2be7a0147452111dc
EMMANUEL <dammylola299@gmail.com>
2022-12-20 22:02:33 +0100

    Created a new folder for the components

---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h2>Sorry!!!</h2>
+            <p>Can't find the resource url...</p>
+            <Link to='/'>Back to the Homepage...</Link>
+        </div>
+    );
+}
+ 
+export default NotFound;
