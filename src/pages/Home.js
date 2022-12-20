fc0c0afc2a80ae2f0976c68c96cfb894835103c4
EMMANUEL <dammylola299@gmail.com>
2022-12-20 22:05:27 +0100

    Created a new folder for the web pages

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import BlogList from "../components/BlogList";
+import useFetch from "../hooks/useFetch";
+
+
+const Home = () => {
+    const { documents: blogs, isPending } = useFetch('blogs');
+
+    return(
+        <div className="home">
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
+        </div>
+    );
+}
+
+export default Home;
+
+
+
