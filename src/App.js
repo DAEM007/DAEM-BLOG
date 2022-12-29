b016ae8772629ce199ae2366e35a849e8cdca5e4
EMMANUEL <dammylola299@gmail.com>
2022-12-29 17:38:49 +0100

    created routes for the new pages like login and signup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 import Create from './pages/Create';
 import BlogDetails from './pages/BlogDetails';
 import NotFound from './components/NotFound';
@@ -14,6 +16,8 @@ function App() {
         <div className="content">
           <Switch>
             <Route exact path='/' component={Home}/>
+            <Route path='/Login' component={Login}/>
+            <Route path='/SignUp' component={SignUp}/>
             <Route path='/create' component={Create}/>
             <Route path='/blogs/:id' component={BlogDetails}/>
             <Route path='/*' component={NotFound}/>
