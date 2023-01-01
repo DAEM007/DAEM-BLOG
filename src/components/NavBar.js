d29156f0bc9c89111d3e1795926eff5f730d67f5
EMMANUEL <dammylola299@gmail.com>
2023-01-01 22:56:54 +0100

    Added the logout button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,34 @@
 // styles import
 import './NavBar.css'
-
+// all react imports
 import { Link } from "react-router-dom";
+// all firebase imports
+import { auth } from "../firebase/Firebase";
+import { signOut } from "firebase/auth";
+
 
 const NavBar = () => {
+
+    const onLogOut = (e) => {
+        e.preventDefault();
+
+        signOut(auth)
+            .then(() => {
+                console.log('user signed out');
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+
+    }
+
     return(
         <nav className="navbar">
             <h1>DAEM's Blog</h1>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/Login">Login</Link>
+                <button onClick={onLogOut}>Log out</button>
                 {/* <Link to="/create">New Blog</Link> */}
             </div>
         </nav>
