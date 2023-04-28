23772ed7bc754cbb113000068b80acb1f4dd9f10
Emmanuel <dammylola299@gmail.com>
2023-04-28 03:10:08 +0100

    updated Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,7 @@ import useFetch from "../hooks/useFetch";
 
 const Home = () => {
     const { documents: blogs, isPending } = useFetch('blogs');
-
     
-
     return(
         <div className="home">
             {isPending && <div>Loading...</div>}
